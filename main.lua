72f57282376059483ca408b5fd4084415f09c6d5
Brenno Pereira Rodrigues <brennopereira6@gmail.com>
2021-02-26 16:17:26 -0300

    readable code

--- main.lua
+++ main.lua
@@ -1,38 +1,39 @@
-local ffi = require("ffi")
-ffi.cdef[[
-typedef struct { uint8_t r, g, b; } color;
-]]
 
-local keys = {}
-keys['e'] = function()
-  for i=1,gridSize do
-    Grid[i] = {0, 0, 0}
-    print(i)
+local ffi = require("ffi")
+
+ffi.cdef([[
+typedef struct { uint8_t r, g, b; } color;
+]])
+
+ local Debug = true
+ local WindowWidth, WindowHeight = love.window.getMode( )
+ local PixelSize = 25
+
+ local ColumnSize, RowSize, GridSize
+
+ local Color = {0, 0, 0}
+
+ local Grid = {}
+
+function love.load()
+  boot()
+end
+
+function boot()
+  ColumnSize = math.floor( WindowHeight / PixelSize )
+  RowSize = math.floor( WindowWidth / PixelSize )
+  GridSize = ColumnSize * RowSize
+  
+  Grid = ffi.new("color[?]", GridSize)
+  for i = 1, GridSize do
+    Grid[i].r = 0
+    Grid[i].g = 0
+    Grid[i].b = 0
+    
   end
 end
-keys['escape'] = function() love.event.push('quit') end
-keys['n'] = function()
-  pixelSize = pixelSize + 5
-  boot()
-end
-keys['m'] = function()
-  pixelSize = pixelSize - 5
-  boot()
-end
-keys['q'] = function() Color = {0, 0, 0} end
-keys['w'] = function() Color = {1, 1, 1} end
-keys['a'] = function() Color = {1, 0, 0} end
-keys['s'] = function() Color = {0, 1, 0} end
-keys['d'] = function() Color = {0, 0, 1} end
-  
-width, height = love.window.getMode( )
-pixelSize = 25
 
-function love.load() --Declare function
-  boot()
-end
 
---dt is the delta time between tha last and current call of update()
 function love.update(dt)
   if love.mouse.isDown(1) then 
     local index = coordsToPixel(love.mouse.getPosition())
@@ -40,7 +41,10 @@ function love.update(dt)
     Grid[index].g = Color[2]
     Grid[index].b = Color[3]
   end
+  
+  
   local touches = love.touch.getTouches( )
+  
   for i, id in ipairs(touches) do
     local x, y = love.touch.getPosition(id)
     love.graphics.circle("fill", x, y, 20)
@@ -52,17 +56,20 @@ function love.update(dt)
 end
 
 function love.draw()
-  for i=1, gridSize do
+  for i = 1, GridSize do
     local x, y = indexToCoords(i)
 
     love.graphics.setColor({0.1 ,0.1 ,0.1})
-    love.graphics.rectangle('line', x, y, width/rowSize, height/columnSize)
+    love.graphics.rectangle('line', x, y, RowSize, ColumnSize)
     
     love.graphics.setColor({Grid[i].r, Grid[i].g, Grid[i].b})
-    love.graphics.rectangle('fill', x, y, width/rowSize, height/columnSize)
-    
-    print('FPS: ' .. love.timer.getFPS())
-    print('Memory usage: ' .. math.floor(collectgarbage 'count') .. 'kb')
+    love.graphics.rectangle('fill', x, y, RowSize, ColumnSize)
+  end
+  
+  if Debug then
+    love.graphics.setColor({1 ,1 ,1})
+    love.graphics.print('FPS: ' .. love.timer.getFPS(), 10, 10)
+    love.graphics.print('Memory usage: ' .. math.floor(collectgarbage 'count') .. 'kb', 10, 30)
   end
 end
 
@@ -75,9 +82,9 @@ end
 function coordsToPixel(x, y)
   
   local index = (
-    math.floor( y / pixelSize ) * rowSize
+    math.floor( y / PixelSize ) * RowSize
   ) + (
-    math.floor( x / pixelSize )
+    math.floor( x / PixelSize )
   )
   
   return index + 1
@@ -85,28 +92,39 @@ end
 
 function indexToCoords(index)
   index = index - 1
-  local x = ( index % rowSize ) * pixelSize
-  local y = math.floor( index / rowSize ) * pixelSize
+  local x = ( index % RowSize ) * PixelSize
+  local y = math.floor( index / RowSize ) * PixelSize
   
   return x, y
-end
-
-function boot()
-  Color = {0, 0, 0}
-  print(pixelSize)
-  rowSize = math.floor(width/pixelSize)
-  columnSize = math.floor(height/pixelSize)
-  gridSize = columnSize*rowSize
-
-  Grid = ffi.new("color[?]", gridSize)
-  for i=1,gridSize do
-    Grid[i].r = 0
-    Grid[i].g = 0
-    Grid[i].b = 0
-    --print(i)
-  end
 end
 
 function love.keypressed(key)
   if keys[key] then keys[key]() end
 end
+
+keys = {}
+keys['e'] = function() --erase grid
+  for i=1,gridSize do
+    Grid[i] = {0, 0, 0}
+    print(i)
+  end
+end
+
+keys['space'] = function() Debug = not Debug end
+
+keys['escape'] = function() love.event.push('quit') end
+
+keys['n'] = function()
+  PixelSize = PixelSize + 5
+  boot()
+end
+keys['m'] = function()
+  PixelSize = PixelSize - 5
+  boot()
+end
+
+keys['q'] = function() Color = {0, 0, 0} end --black
+keys['w'] = function() Color = {1, 1, 1} end --white
+keys['a'] = function() Color = {1, 0, 0} end --red
+keys['s'] = function() Color = {0, 1, 0} end --geeen
+keys['d'] = function() Color = {0, 0, 1} end --blue
